e92d94daca50565eacebac44f874c2c155876f60
Luc <lucborris@gmail.com>
2021-03-03 14:03:32 -0500

    Add dashboard skeleton styling required

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["ADMIN_USERNAME"], password: ENV["ADMIN_PASSWORD"]
 
   def show
+    @categories = Category.all
+    @products = Product.all
   end
 end
