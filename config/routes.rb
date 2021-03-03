398c6e1826b7d766f8ca16377443d080f4c6c810
Borrisson <lucborris@gmail.com>
2021-03-03 13:39:53 +0000

    Add line_items route via generate controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,20 @@
 Rails.application.routes.draw do
-
-  root to: 'products#index'
+  root to: "products#index"
 
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
 
   resource :cart, only: [:show] do
-    post   :add_item
-    post   :remove_item
+    post :add_item
+    post :remove_item
   end
 
-  resources :orders, only: [:create, :show]
+  resources :orders, only: [:create, :show] do
+    resources :line_items
+  end
 
   namespace :admin do
-    root to: 'dashboard#show'
+    root to: "dashboard#show"
     resources :products, except: [:edit, :update, :show]
   end
 
