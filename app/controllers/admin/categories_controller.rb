ffd1e2d352de2f2884b52b8c7569bdaafd878b05
Borrisson <lucborris@gmail.com>
2021-03-04 13:00:34 +0000

    Add Categories#create action logic

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -7,5 +7,12 @@ class Admin::CategoriesController < ApplicationController
   end
 
   def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: "Category created!"
+    else
+      render :new
+    end
   end
 end
