6a4c87e7a9e316c27e4926d9979721100bb5a6ea
Borrisson <lucborris@gmail.com>
2021-03-04 13:10:58 +0000

    Configure except actions of admin/categories resources

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: "dashboard#show"
     resources :products, except: [:edit, :update, :show]
-    resources :categories, except: [:destroy, :edit]
+    resources :categories, except: [:destroy, :update, :edit, :show]
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
