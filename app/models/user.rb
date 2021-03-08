839e5d9ca2f57cc1f896e23235f001542438f487
Borrisson <lucborris@gmail.com>
2021-03-08 20:36:55 +0000

    Update user model to have first and last name instead of name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
   has_secure_password
-  validates :name, presence: true
-  validates :email, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
   validates :password, presence: true, confirmation: true
   validates :password_confirmation, presence: true
 end
