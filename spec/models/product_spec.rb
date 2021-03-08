98278180d973ea8a321689eed2520a6a682686a5
Borrisson <lucborris@gmail.com>
2021-03-08 15:00:06 +0000

    Add presence tests for product models

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,14 +2,35 @@ require "rails_helper"
 
 RSpec.describe Product, type: :model do
   describe "Validations" do
-    context "To be present" do
-      it "has name" do
+    context "Must Be Present" do
+      it "saves with all fields present" do
+        @mock_category = Category.new(:name => "Electronics")
+        @product = Product.new(:name => "Luc", :price => 10000, :quantity => 3, :category => @mock_category)
+
+        expect(@product.save!).to be true
       end
-      it "has price" do
+      it "name" do
+        @mock_category = Category.new(:name => "Electronics")
+        @product = Product.new(:name => nil, :price => 10000, :quantity => 3, :category => @mock_category)
+
+        expect(@product.save).to be false
       end
-      it "has quantity" do
+      it "price" do
+        @mock_category = Category.new(:name => "Electronics")
+        @product = Product.new(:name => "Luc", :price => nil, :quantity => 3, :category => @mock_category)
+
+        expect(@product.save).to be false
       end
-      it "has category" do
+      it "quantity" do
+        @mock_category = Category.new(:name => "Electronics")
+        @product = Product.new(:name => "Luc", :price => 10000, :quantity => nil, :category => @mock_category)
+
+        expect(@product.save).to be false
+      end
+      it "category" do
+        @product = Product.new(:name => "Luc", :price => 10000, :quantity => 3, :category => nil)
+
+        expect(@product.save).to be false
       end
     end
   end
