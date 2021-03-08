73818df374592c337adb954a7448e1fd710b0bad
Borrisson <lucborris@gmail.com>
2021-03-08 14:24:06 +0000

    Add tests description ONLY for Product model

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,5 +2,15 @@ require "rails_helper"
 
 RSpec.describe Product, type: :model do
   describe "Validations" do
+    context "To be present" do
+      it "has name" do
+      end
+      it "has price" do
+      end
+      it "has quantity" do
+      end
+      it "has category" do
+      end
+    end
   end
 end
