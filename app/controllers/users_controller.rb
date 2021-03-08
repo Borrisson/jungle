d3e745257e0aad870cb656ae5d371abee9add05b
Borrisson <lucborris@gmail.com>
2021-03-08 20:52:58 +0000

    Update controller and views to use first_name and last_name

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      session[:user_id] = user.id
+      session[:user_id] = @user.id
       redirect_to "/"
     else
       redirect_to "/signup", notice: "Either the password did not match or the email is already in use"
@@ -15,6 +15,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
   end
 end
