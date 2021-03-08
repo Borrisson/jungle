fa307c7b2174129620e38b1e67660bbe3df12ebe
Borrisson <lucborris@gmail.com>
2021-03-08 19:56:04 +0000

    Add changes to pass model tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :password, presence: true, confirmation: true
+  validates :password_confirmation, presence: true
 end
