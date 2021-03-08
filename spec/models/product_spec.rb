fc230d84ac4b598354d2eac26145608cd4d35a7c
Borrisson <lucborris@gmail.com>
2021-03-08 19:12:42 +0000

    Add test cases for Product Model

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,35 +2,31 @@ require "rails_helper"
 
 RSpec.describe Product, type: :model do
   describe "Validations" do
+    subject { Product.new(:name => "Luc", :price => 10000, :quantity => 3, :category => Category.new(:name => "Electronics")) }
+    before { subject.save! }
     context "Must Be Present" do
-      it "saves with all fields present" do
-        @mock_category = Category.new(:name => "Electronics")
-        @product = Product.new(:name => "Luc", :price => 10000, :quantity => 3, :category => @mock_category)
-
-        expect(@product.save!).to be true
+      it "will save with all fields present" do
+        expect(subject).to be_valid
       end
-      it "name" do
-        @mock_category = Category.new(:name => "Electronics")
-        @product = Product.new(:name => nil, :price => 10000, :quantity => 3, :category => @mock_category)
-
-        expect(@product.save).to be false
+      it "is not valid without a name" do
+        subject.name = nil
+        expect(subject).to_not be_valid
+        expect(subject.errors.messages[:name]).to eq ["can't be blank"]
       end
-      it "price" do
-        @mock_category = Category.new(:name => "Electronics")
-        @product = Product.new(:name => "Luc", :price => nil, :quantity => 3, :category => @mock_category)
-
-        expect(@product.save).to be false
+      it "is not valid without a price" do
+        subject.price_cents = nil
+        expect(subject).to_not be_valid
+        expect(subject.errors.messages[:price_cents]).to eq ["is not a number"]
       end
-      it "quantity" do
-        @mock_category = Category.new(:name => "Electronics")
-        @product = Product.new(:name => "Luc", :price => 10000, :quantity => nil, :category => @mock_category)
-
-        expect(@product.save).to be false
+      it "is not valid without a quantity" do
+        subject.quantity = nil
+        expect(subject).to_not be_valid
+        expect(subject.errors.messages[:quantity]).to eq ["can't be blank"]
       end
-      it "category" do
-        @product = Product.new(:name => "Luc", :price => 10000, :quantity => 3, :category => nil)
-
-        expect(@product.save).to be false
+      it "is not valid without a category" do
+        subject.category = nil
+        expect(subject).to_not be_valid
+        expect(subject.errors.messages[:category]).to eq ["can't be blank"]
       end
     end
   end
